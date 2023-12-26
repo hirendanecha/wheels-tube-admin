2a1c5cd64e18fb38fcc8d03548212b606fef3749
Swapnil Opash <sra.opash@gmail.com>
2023-12-26 18:33:15 +0530

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://dev-api.freedom.buzz/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://dev-api.freedom.buzz/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://freedom-api.opash.in/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://freedom-api.opash.in/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+};
